728821e7eb0529c42a8fcb844d309e8f2816ce2b
Darklight-03 <bo.corman@gmail.com>
2017-05-21 04:02:18 -0500

    added far better arg handling with regex and shit

--- commands/mute.js
+++ commands/mute.js
@@ -4,15 +4,15 @@ const moment = require('moment'); // Part of log writing
 const prism = require('prism-media'); // Prism for smoother file playing of very short files
 const database = require('../database.js');
 
-exports.main = function(bot, msg, timeout, botPerm, userPerm) { // Export command function
+exports.main = function(bot, msg, timeout, botPerm, userPerm, args) { // Export command function
 	var command = "mute"; // For logging purposes
 	if (timeout.check(msg.author.id, msg)) {
 		return;
 	}
 	if(typeof msg.mentions.users.first() !== 'undefined'){
 		// Check for cooldown, if on cooldown notify user of it and abort command execution.
-		let args = msg.content.substr(config.commandPrefix.length + command.length + 1 + config.needsSpace);
-		let lengthMute = parseInt(args.trim().split(' ')[1])*1000;
+		args = args[1];
+		let lengthMute = parseInt(args)*1000;
 		if(typeof lengthMute == 'undefined'||isNaN(lengthMute)){
 			lengthMute = 0;
 			console.log('undef lm');
