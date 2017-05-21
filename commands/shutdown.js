88c348b17aa6555a75a530c551b46c60d38cf02b
Darklight-03 <bo.corman@gmail.com>
2017-05-21 04:35:48 -0500

    fixed config.json.example and removed logging from shutdown

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -17,8 +17,6 @@ exports.main = function(bot, msg, timeout, botPerm, userPerm) { // Export comman
 		return; // ...and abort command execution.
 	}
 	msg.reply(`${bot.user.username} shutting down! Bye!`);
-	fs.appendFileSync(`${config.logPath}${config.shutdownLog}`, `\n[${moment().format('DD/MM/YYYY HH:mm:ss')}][POWER] ${msg.author.username}#${msg.author.discriminator} successfully used the "${msg.content.substr(config.commandPrefix.length + 1, command.length)}" command on the '${msg.guild}' server!`); // Log command use, when and by whom
-	console.log(`${bot.user.username} shutting down! (${msg.author.username}#${msg.author.discriminator} on '${msg.guild}')`);
 	setTimeout(function() {
 		// Define timeout for bot shutdown, in which...
 		bot.destroy();
