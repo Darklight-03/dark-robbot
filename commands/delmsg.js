003bcf53ee50a2bd37eec28acca2a58240dd72ee
Darklight-03 <bo.corman@gmail.com>
2017-05-28 22:16:51 -0500

    added delmsg

---
+++ commands/delmsg.js
@@ -0,0 +1,37 @@
+const config = require('../config.json');
+const say = require('./Basic tasks/say.js');
+
+exports.main = function(bot, msg, timeout, botPerm, userPerm, args){
+    let server = msg.guild;
+    let s = '';
+    let time = Date.now();
+    let minus = parseInt(args[0].substring(0,args[0].length-1));
+    let type = args[0].charAt(args[0].length-1);
+    let fintime;
+    if(type=='m'){
+        fintime = time - minus*1000*60;
+        typehuman = 'minutes';
+    }
+    if(type == 's'){
+        fintime = time - minus*1000;
+        typehuman = 'seconds';
+    }
+    if(type == 'h'){
+        fintime = time - minus*1000*60*60;
+        typehuman = 'hours';
+    }
+    if(!fintime){
+        return;
+    }
+    msg.channel.fetchMessages({limit: 100}).then(messages => {
+		console.log(`Received ${messages.size} messages`);
+		messages.forEach((messagee,i)=>{
+            if(messagee.createdTimestamp>fintime){
+                setTimeout(()=>{
+                    messagee.delete();
+                },1000*i);
+            }
+		});
+	});
+    say.reply(msg,`removed messages from up to ${minus} ${typehuman} ago`);
+};
