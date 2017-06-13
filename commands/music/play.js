c42e70313beb333a47ddee0ed214c1e88e4942a3
Darklight-03 <bo.corman@gmail.com>
2017-06-13 15:18:41 -0500

    final queueless commit for music

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,9 +1,9 @@
-const config = require('../config.json'); // Import configuration
+const config = require('../../config.json'); // Import configuration
 const fs = require('fs'); // For log writing
 const moment = require('moment'); // Part of log writing
 const prism = require('prism-media'); // Prism for smoother file playing of very short files
-const database = require('../database.js');
-const say = require('./Basic tasks/say.js');
+const database = require('../../database.js');
+const say = require('../Basic tasks/say.js');
 
 const youtubedl = require('youtube-dl');
 
@@ -13,25 +13,25 @@ exports.main = function (bot, msg, timeout, botPerm, userPerm, args) { // Export
 
     if (!args[0].toLowerCase().startsWith('http')) {
         args[0] = 'gvsearch1:' + args[0];
-        msg.reply(args[0]);
     }
     var vid;
-    msg.reply('Searching...').then(response => {
+    say.reply(msg, 'Searching...').then(response => {
         youtubedl.getInfo(args[0], ['-q', '--no-warnings', '--force-ipv4'], (err, info) => {
             if (err || info.format_id === undefined || info.format_id.startsWith('0')) {
-                msg.reply('Invalid video!');
+                say.reply(msg, 'Invalid video!');
                 return;
             }
-            msg.reply(info.url);
             vid = info;
-            msg.guild.channels.forEach((channel) => {
-                if (channel.type == 'voice' && channel.id == '185579003837546496') {
-                    channel.join().then(connection => {
-                        connection.playStream(vid.url);
-                        msg.reply('done');
-                    });
-                }
-            });
+            channel = msg.guild.channels.get('185579003837546496');
+            if (channel.type == 'voice') {
+                channel.join().then(connection => {
+                    voice = connection.playStream(vid.url, { volume: .3 });
+                    voice.on('end', () => {
+                        connection.disconnect();
+                    })
+                    say.reply(msg, 'Now Playing '+vid.title);
+                });
+            }
         });
     });
 
