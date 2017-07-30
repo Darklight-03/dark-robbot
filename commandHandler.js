476307a175159675bbbc215fbab5c69b70bb8ab3
Darklight-03 <bo.corman@gmail.com>
2017-07-29 21:34:09 -0500

    mute now uses same format as delmsg

--- commandHandler.js
+++ commandHandler.js
@@ -5,17 +5,17 @@ const config = require('./config.json'); // Import configuration
 	and arg into array, convert to lowercase and select the command part ([0] of the array)
 	and run command
 */
-exports.runCommand = function(bot,msg,timeout,commands){
+exports.runCommand = function (bot, msg, timeout, commands) {
 	const botPerm = msg.guild.member(bot.user); // For permission checking on the bot's side later on in the commands
 	const userPerm = msg.guild.member(msg.member.user); // For permission checking on the user's side later on in the commands
 	var command = msg.content.replace(config.commandPrefix, '').trim().split(' ')[0].toLowerCase();
-	var args = msg.content.replace(config.commandPrefix,'').replace(command,'');
-	var matches = args.match(new RegExp(/"([^"]*)"|'([^']*)'|[^\s]+/,'g'));
+	var args = msg.content.replace(config.commandPrefix, '').replace(command, '');
+	var matches = args.match(new RegExp(/"([^"]*)"|'([^']*)'|[^\s]+/, 'g'));
 	var argsfinal = [];
-	if(!(!(matches))){
+	if (!(!(matches))) {
 		matches.forEach((i) => {
-			if(i[0]==i[0]){
-				if(i[0]=='\''||i[0]=='\"') str = i.substring(1,i.length-1); else str = i;
+			if (i[0] == i[0]) {
+				if (i[0] == '\'' || i[0] == '\"') str = i.substring(1, i.length - 1); else str = i;
 			}
 			argsfinal.push(str);
 		});
@@ -24,9 +24,9 @@ exports.runCommand = function(bot,msg,timeout,commands){
 	if (Object.keys(commands).indexOf(command) > -1) {
 		// If the given command is an actual command that is available...
 		commands[command].main(bot, msg, timeout, botPerm, userPerm, argsfinal);
-		setTimeout(()=>{
-        	msg.delete();
-        },1000*60*3);
+		setTimeout(() => {
+			msg.delete();
+		}, 1000 * 60 * 3);
 		// ...run the command.
 	}
 };
--- commands/mute.js
+++ commands/mute.js
@@ -5,20 +5,36 @@ const prism = require('prism-media'); // Prism for smoother file playing of very
 const database = require('../database.js');
 const say = require('./Basic tasks/say.js');
 
-exports.main = function(bot, msg, timeout, botPerm, userPerm, args) { // Export command function
+exports.main = function (bot, msg, timeout, botPerm, userPerm, args) { // Export command function
 	var command = "mute"; // For logging purposes
 	if (timeout.check(msg.author.id, msg)) {
 		return;
 	}
-	if(typeof msg.mentions.users.first() !== 'undefined'){
+	if (typeof msg.mentions.users.first() !== 'undefined') {
 		// Check for cooldown, if on cooldown notify user of it and abort command execution.
-		args = args[1];
-		let lengthMute = parseInt(args)*1000;
-		if(typeof lengthMute == 'undefined'||isNaN(lengthMute)){
-			lengthMute = 0;
-			console.log('undef lm');
+		time  =  parseInt(args[1].substring(0,args[1].length-1));;
+		type=  args[1].charAt(args[1].length-1);
+		// let lengthMute = parseInt(args) * 1000;
+		// if (typeof lengthMute == 'undefined' || isNaN(lengthMute)) {
+		// 	lengthMute = 0;
+		// 	console.log('undef lm');
+		// }
+		let lengthMute;
+		if (type == 'm') {
+			lengthMute = time * 1000 * 60;
+			typehuman = 'minutes';
 		}
-		let arg2 = lengthMute;
+		if (type == 's') {
+			lengthMute = time * 1000;
+			typehuman = 'seconds';
+		}
+		if (type == 'h') {
+			lengthMute = time * 1000 * 60 * 60;
+			typehuman = 'hours';
+		}
+		if (!lengthMute) {
+			return;
+		}
 		let users = msg.guild.members;
 		let mutee = users.get(msg.mentions.users.first().id);
 		let muted = msg.guild.roles.find("name", 'Muted').id;
@@ -26,10 +42,10 @@ exports.main = function(bot, msg, timeout, botPerm, userPerm, args) { // Export 
 
 		try {
 			if (!msg.member.hasPermission("KICK_MEMBERS")) {
-				say.reply(msg,"U R NOT A MODERATOR");
+				say.reply(msg, "U R NOT A MODERATOR");
 			} else {
 				if (mutee.roles.has(muted)) {
-					say.reply(msg,"that user is already muted");
+					say.reply(msg, "that user is already muted");
 				} else {
 					//mute them.
 					mutee.addRole(muted);
@@ -38,13 +54,13 @@ exports.main = function(bot, msg, timeout, botPerm, userPerm, args) { // Export 
 							channel.delete();
 						});
 					});
-					lengthMute = lengthMute+Date.now();
+					lengthMute = lengthMute + Date.now();
 					database.addMuted(mutee.id, msg.guild.id, lengthMute);
-					say.reply(msg,"muted " + mutee.toString() + ' for ' + arg2 / 1000 + ' seconds.');
+					say.reply(msg, `muted ${mutee.toString()} for ${time} ${typehuman}`);
 				}
 			}
 		} catch (err) {
-			say.reply(msg,"failed, " + err.toString());
+			say.reply(msg, "failed, " + err.toString());
 		}
 	}
 };
