c4ea93f12d6e2b6bfcc8e1b2da3dc74a12371d2f
Darklight-03 <bo.corman@gmail.com>
2017-09-13 17:00:01 -0500

    actually fixed songs cutting out now (i hope...?)

--- commands/music/play.js
+++ commands/music/play.js
@@ -8,20 +8,21 @@ const musicManager = require('../../musicManager.js');
 const ytdl = require('ytdl-core');
 const youtubedl = require('youtube-dl');
 
-exports.main = function (bot, msg, timeout, botPerm, userPerm, args) { // Export command function
-
+var msg;
+exports.main = function (bot, message, timeout, botPerm, userPerm, args) { // Export command function
+    msg = message;
     if (!args[0]) {
         return;
     }
     search = args.join(' ');
     if (!search.toLowerCase().startsWith('http')) {
-        srch = search;
+        var srch = search;
         search = 'gvsearch1:' + search;
         var vid;
-        say.reply(msg, `Searching google for ${srch.replace('@','')}...`).then(response => {
+        say.reply(msg, `Searching google for ${srch.replace('@', '')}...`).then(response => {
             youtubedl.getInfo(search, ['-q', '--no-warnings', '--force-ipv4', '-f bestaudio'], (err, info) => {
                 if (err || info.format_id === undefined || info.format_id.startsWith('0')) {
-                    say.reply(msg, `Invalid video ${search.replace('@','')}!\n${err}`);
+                    say.reply(msg, `Invalid video ${search.replace('@', '')}!\n${err}`);
                     return;
                 }
                 vid = info;
@@ -33,26 +34,36 @@ exports.main = function (bot, msg, timeout, botPerm, userPerm, args) { // Export
         queueUrl(search, msg, bot);
     }
 };
-function queueUrl(url, msg, bot){
+function queueUrl(url, msg, bot) {
     var vid;
     say.reply(msg, 'Searching...').then(response => {
-        ytdl.getInfo(url, {}, (err, info) => {
+        
+        ytdl.getInfo(url, {}, function (err,info){
             if (err) {
                 say.reply(msg, `Invalid video ${url.replace('@', '')}!\\${err}`);
                 console.log(info);
                 console.log(err);
-
+        
                 console.log(info.format_id);
                 return;
             }
             purl = info.formats.find(function (format) {
                 return format.type.includes('audio');
             });
-            vid = { url: purl.url, title: info.title };
-            say.reply(msg, `added ${vid.title} to queue, ${vid.url}`);
-            musicManager.addQueue(vid, msg, bot);
+            
+            say.reply(msg, `added ${info.title} to queue, ${purl.url}`).then(()=>{
+                const soong = ytdl(url, {retries: 25, filter: (format) => !format.bitrate && format.audioEncoding === 'opus' }).on('error', err => console.log(err));
+                var vid = { url: soong, title: info.title };
+                musicManager.addQueue(vid, msg, bot);
+            });
+            
         });
+        
+        
     });
+}
+function getinfourl(url, msg, bot){
+    
 }
 
 
