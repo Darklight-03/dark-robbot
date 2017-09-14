b005f8256722119b240775666090820ba427d364
Darklight-03 <bdc3142@gmail.com>
2017-09-14 14:17:32 -0500

    now should restart after updating

--- robbot.js
+++ robbot.js
@@ -167,7 +167,9 @@ if (cluster.isWorker) {
 	github.on('*', function (event, repo, ref, data) {
 		console.log('detetected update... downloading...');
 		var exec = require('child_process').exec;
-		exec('git pull', function callback(error, stdout, stderr){
+		exec('"git pull"', function callback(error, stdout, stderr){
+			console.log('complete! restarting..');
+			process.exit(0);
 			// runs git updater
 		});
 	});
