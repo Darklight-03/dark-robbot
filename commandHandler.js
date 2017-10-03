c5bc66acb4426cb4698cd93d7cee1396dac25611
Darklight-03 <bdc3142@gmail.com>
2017-10-03 14:51:24 -0500

    fixed using capital letters in commands causing the command itself to also be the first argument

--- commandHandler.js
+++ commandHandler.js
@@ -8,8 +8,9 @@ const config = require('./config.json'); // Import configuration
 exports.runCommand = function (bot, msg, timeout, commands) {
 	const botPerm = msg.guild.member(bot.user); // For permission checking on the bot's side later on in the commands
 	const userPerm = msg.guild.member(msg.member.user); // For permission checking on the user's side later on in the commands
-	var command = msg.content.replace(config.commandPrefix, '').trim().split(' ')[0].toLowerCase();
+	var command = msg.content.replace(config.commandPrefix, '').trim().split(' ')[0];
 	var args = msg.content.replace(config.commandPrefix, '').replace(command, '');
+	command = command.toLowerCase();
 	var matches = args.match(new RegExp(/"([^"]*)"|'([^']*)'|[^\s]+/, 'g'));
 	var argsfinal = [];
 	if (!(!(matches))) {
@@ -19,7 +20,7 @@ exports.runCommand = function (bot, msg, timeout, commands) {
 			}
 			argsfinal.push(str);
 		});
-		console.log(argsfinal);
+		console.log(command+" "+argsfinal);
 	}
 	if (Object.keys(commands).indexOf(command) > -1) {
 		// If the given command is an actual command that is available...
